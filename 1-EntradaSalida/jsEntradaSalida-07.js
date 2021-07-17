e13e14801c9cd06ce8c5962b279ebbbd8942018c
Sharlenne <sharlenne8@gmail.com>
2021-07-17 19:51:09 -0300

    Cambio Nuevo

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+	numeroDos = parseInt(numeroDos);
+
+	suma = numeroUno + numeroDos;
+
+	alert("La suma es "+suma);
+
 }
 
 function restar()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var resta;
+
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+	numeroDos = parseInt(numeroDos);
+
+	resta = numeroUno - numeroDos;
+
+	alert("La resta es "+resta);
+
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno;
+	var numeroDos;
+	var producto;
+
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+	numeroDos = parseInt(numeroDos);
+
+	producto = numeroUno * numeroDos;
+
+	alert("El producto es "+producto);
+
 }
 
 function dividir()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var cociente; 
+
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+	numeroDos = parseInt(numeroDos);
+
+	cociente = numeroUno / numeroDos;
+
+	alert("El cociente es "+cociente);
+
 }
 
