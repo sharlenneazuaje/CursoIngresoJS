70f742ff053345f09d4b4126fe4aaf448f38ed9c
Sharlenne <sharlenne8@gmail.com>
2021-07-17 15:15:59 -0300

    cambio nuevo

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,11 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+
+	nombre = prompt("Ingresar un nombre", "Yoraima Torres");
+
+	alert("Su nombre es "+nombre);
+
 }
 
