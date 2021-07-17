f9252667dbfb44c7657d0de2ad67e07690aa5d6a
Sharlenne <sharlenne8@gmail.com>
2021-07-17 20:30:08 -0300

    Cambio Nuevo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe;
+	var resultado;
+
+	importe = document.getElementById("txtIdImporte").value;
+	importe = parseInt(importe);
+
+	resultado = importe - importe * 25 / 100;
+
+	document.getElementById("txtIdResultado").value = resultado;
 }
