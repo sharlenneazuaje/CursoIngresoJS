402d204058dc8510a2d61bd8a1a55e5429133c8f
Sharlenne <sharlenne8@gmail.com>
2021-07-17 20:01:42 -0300

    Cambio Nuevo

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,20 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	var dividendo;
+	var divisor;
+	var modulo;
+	var mensaje;
+
+	dividendo = document.getElementById("txtIdNumeroDividendo").value;
+	dividendo = parseInt(dividendo);
+	divisor = document.getElementById("txtIdNumeroDivisor").value;
+	divisor = parseInt(divisor);
+
+	modulo = dividendo % divisor;
+
+	mensaje = "El resto es  "+modulo;
+
+	alert(mensaje);
+
 }
