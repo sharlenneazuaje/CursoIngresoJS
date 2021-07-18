5986263fed5e161480644e790666381d1658c665
Sharlenne <sharlenne8@gmail.com>
2021-07-18 17:20:25 -0300

    Cambio Nuevo

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,67 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacion;
+	var destino; 
+	var porcentaje;
+	var precioBase;
+	var precioFinal;
 
-}//FIN DE LA FUNCIÓN
+	
+	estacion = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	precioBase = 15000;
+
+	switch(estacion)
+	{
+		case "Invierno":
+			switch(destino)
+			{
+				case "Bariloche":
+					porcentaje = +20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje = -10;
+					break;
+				case "Mar del plata": 
+					porcentaje = -20;
+					break;
+			}
+			break;
+		
+		case "Verano": 
+			switch(destino)
+			{
+				case "Bariloche":
+					porcentaje = -20;
+					break;
+				case "Cataratas":
+				case "Cordoba":
+					porcentaje = +10;
+					break;
+				case "Mar del plata":
+					porcentaje = +20;
+					break;
+			}
+			break;
+
+		default:
+			switch(destino)
+			{
+				case "Bariloche":
+				case "Cataratas":
+				case "Mar del plata":
+					porcentaje = +10;
+					break;
+				case "Cordoba":
+					porcentaje = 0;
+					break;
+			}
+			break;
+	}
+
+	precioFinal = precioBase + precioBase * porcentaje / 100; 
+	alert("El precio final es "+precioFinal+"$");
+
+}
