fb98e65f3bbe8ea92b8dbf27641017e82c738b4c
Sharlenne <sharlenne8@gmail.com>
2021-07-17 21:13:07 -0300

    Cambio Nuevo

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	var edad;
 
-}//FIN DE LA FUNCIÓN
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+
+	if(edad > 17)
+	{
+		alert("Es mayor de edad");
+	}
+	
+}
