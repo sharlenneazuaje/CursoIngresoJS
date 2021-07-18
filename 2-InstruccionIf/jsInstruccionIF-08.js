664325589f184f56bb1ecdc89a6d5379c3bb761e
Sharlenne <sharlenne8@gmail.com>
2021-07-17 23:02:27 -0300

    Cambio Nuevo

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	var edad;
+	var estadoCivil;
 
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+	estadoCivil = document.getElementById("estadoCivil").value;
 
-}//FIN DE LA FUNCIÓN
+	if(edad > 17 && estadoCivil == "Soltero")
+	{
+		alert("Es soltero y NO es menor");
+	}
+
+}
