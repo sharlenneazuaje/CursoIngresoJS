9bfba9566078885790164324174dd8e72720aaa7
Sharlenne <sharlenne8@gmail.com>
2021-07-17 23:05:39 -0300

    Cambio Nuevo

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,8 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	var numero;
 
-}//FIN DE LA FUNCIÓN
+	numero = Math.floor(Math.random() * 10) + 1;
+
+	alert(numero);
+}
