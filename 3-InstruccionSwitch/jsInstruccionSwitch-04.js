e990ec8219640d0b1ec836bb9cd99abfa5393d09
Sharlenne <sharlenne8@gmail.com>
2021-07-18 11:41:37 -0300

    Cambio Nuevo

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,31 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	var mes; 
+	var mensaje;
 
+	mes = document.getElementById("txtIdMes").value;
 
+	switch(mes)
+	{
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre": 
+		case "Diciembre":
+			mensaje = "Tiene 31 días";
+			break;
+		case "Febrero":
+			mensaje = "Tiene 28 días";
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			mensaje = "Tiene 30 días";
+			break;
+	}
+	alert(mensaje);
 
-}//FIN DE LA FUNCIÓN
+}
