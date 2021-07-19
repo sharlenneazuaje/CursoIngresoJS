7ae53f8925d7d8c3a624fb4fbce546089a25c1c0
Sharlenne <sharlenne8@gmail.com>
2021-07-18 21:36:23 -0300

    Cambio Nuevo

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,101 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    var cantidadLamparas;
+    var descuento;
+    var marca;
+    var precioSinDescuento;
+    var precioConDescuento;
+    var importeFinal; 
+    var impuesto; 
+   
+    cantidadLamparas = document.getElementById("txtIdCantidad").value;
+    cantidadLamparas = parseInt(cantidadLamparas);
+    marca = document.getElementById("Marca").value;
+
+
+    if(marca == "ArgentinaLuz")
+    {
+        switch(cantidadLamparas)
+        {
+            case 1:
+            case 2:
+                descuento = 0;
+                break;
+            case 3:
+                descuento = 15;
+                break;
+            case 4:
+                descuento = 25;
+                break;
+            case 5:
+                descuento = 40;
+                break;
+            default:
+                descuento = 50;
+                break;
+        }
+    }
+    else
+    {
+        if(marca == "FelipeLamparas")
+        {
+            switch(cantidadLamparas)
+            {
+                case 1:
+                case 2:
+                    descuento = 0;
+                    break;
+                case 3:
+                    descuento = 10;
+                    break;
+                case 4:
+                    descuento = 25;
+                    break;
+                case 5:
+                    descuento = 30;
+                    break;
+                default:
+                    descuento = 50;
+                    break;
+            }
+        }
+        else
+        {
+            switch(cantidadLamparas)
+            {
+                case 1:
+                case 2:
+                    descuento = 0;
+                    break;
+                case 3:
+                    descuento = 5;
+                    break;
+                case 4:
+                    descuento = 20;
+                    break;
+                case 5:
+                    descuento = 30;
+                    break;
+                default:
+                    descuento = 50;
+                    break;
+
+            }
+        }
+    }
+
+    precioSinDescuento = 35 * cantidadLamparas;
+    precioConDescuento = precioSinDescuento - ((35 * cantidadLamparas)  * descuento / 100);
+    importeFinal = precioConDescuento;
+
+    if(importeFinal > 120)
+    {
+        impuesto =  importeFinal * 10 / 100;
+        importeFinal = importeFinal + impuesto;
+        alert("IIBB Usted pago "+importeFinal+", siendo "+impuesto+" el impuesto que se pagó.");
+    }
+
+    document.getElementById("txtIdprecioDescuento").value = importeFinal;
+
 }
