5b24e0dcb8563f03938fbc07414b982d8cc286e0
Sharlenne <sharlenne8@gmail.com>
2021-07-18 21:14:23 -0300

    Cambio Nuevo

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,71 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var mensaje;
+
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio1 = parseInt(precio1);
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio2 = parseInt(precio2);
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+    precio3 =  parseInt(precio3);
+
+    suma = precio1 + precio2 + precio3;
+
+    mensaje = "La suma es "+suma;
+
+    alert(mensaje);
+
 }
 function Promedio () 
 {
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var promedio;
+    var mensaje;
+
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio1 = parseInt(precio1);
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio2 = parseInt(precio2);
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+    precio3 =  parseInt(precio3);
+
+    suma = precio1 + precio2 + precio3;
+    promedio = suma / 3;
+
+    mensaje = "El promedio es "+promedio;
+
+    alert(mensaje);
 	
 }
 function PrecioFinal () 
 {
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var precioFinal;
+    var mensaje;
+
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio1 = parseInt(precio1);
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio2 = parseInt(precio2);
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+    precio3 =  parseInt(precio3);
+
+    suma = precio1 + precio2 + precio3;
+    precioFinal = suma + suma * 21 / 100;
+
+    mensaje = "El precio final con IVA 21% es "+precioFinal;
+
+    alert(mensaje);
 	
 }
