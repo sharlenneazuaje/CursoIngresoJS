f3e9f3bf98425ef61d86ec107a817cf5bbf4a38a
Sharlenne <sharlenne8@gmail.com>
2021-07-18 22:38:57 -0300

    Cambio Nuevo

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
-  
+    var nombre1;
+    var nombre2;
+    var peso1;
+    var peso2;
+    var sumaPesos;
+    var promedioPesos;
+    var mensaje;
+
+    nombre1 = prompt("Ingrese el nombre de la primera persona");
+    nombre2 = prompt("Ingrese el nombre de la segunda persona");
+
+    peso1 = prompt("Ingrese el peso de la primera persona");
+    peso1 = parseInt(peso1);
+    peso2 = prompt("Ingrese el peso de la segunda persona");
+    peso2 = parseInt(peso2);
+
+    sumaPesos = peso1 + peso2;
+    promedioPesos = sumaPesos / 2;
+
+    mensaje = "Ustedes se llaman "+nombre1+" y "+nombre2+", pesan "+peso1+" y "+peso2+" kilos, que sumados son "+sumaPesos+" kilos, y el promedio de peso es "+promedioPesos+".";
+
+    alert(mensaje);
+
 }
