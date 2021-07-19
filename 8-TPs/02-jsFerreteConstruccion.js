be1ea45fd1dea959c6b98495cc6fbd9f44cec15f
Sharlenne <sharlenne8@gmail.com>
2021-07-18 21:33:07 -0300

    Cambio Nuevo

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,61 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+     var largo;
+     var ancho;
+     var perimetro;
+     var cantidadAlambre;
+     var mensaje;
 
+     largo = document.getElementById("txtIdLargo").value;
+     largo = parseInt(largo);
+     ancho = document.getElementById("txtIdAncho").value;
+     ancho = parseInt(ancho);
+
+     perimetro = 2 * (largo + ancho);
+     cantidadAlambre = perimetro * 3;
+
+     mensaje = "La cantidad de Alambre a utilizar es: "+cantidadAlambre;
+
+     alert(mensaje);
 }
 function Circulo () 
 {
-	
+	var radio;
+    var perimetro;
+    var cantidadAlambre;
+    var mensaje;
+    const PI = 3.14;
+
+    radio = document.getElementById("txtIdRadio").value;
+    radio = parseInt(radio);
+
+    perimetro = radio * 2 * PI;
+    cantidadAlambre = perimetro * 3;
+
+    mensaje = "La cantidad de Alambre a utilizar es: "+cantidadAlambre;
+
+    alert(mensaje);
 }
 function Materiales () 
 {
-	
+	var largo;
+    var ancho;
+    var area; 
+    var bolsasCemento;
+    var bolsasCal;
+    var mensaje; 
+    
+    largo = document.getElementById("txtIdLargo").value;
+    largo = parseInt(largo);
+    ancho = document.getElementById("txtIdAncho").value;
+    ancho = parseInt(ancho);
+
+    area = largo * ancho;
+    bolsasCemento = area * 2;
+    bolsasCal = area * 3;
+
+    mensaje = "Se necesitan "+bolsasCemento+" bolsas de Cemento y "+bolsasCal+" bolsas de Cal";
+
+    alert(mensaje);
 }
