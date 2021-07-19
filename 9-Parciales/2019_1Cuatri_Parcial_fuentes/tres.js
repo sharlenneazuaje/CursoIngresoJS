cdaf42587b85a21af067bb3e218b15fb50b381f7
Sharlenne <sharlenne8@gmail.com>
2021-07-18 22:50:49 -0300

    Cambio Nuevo

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+    var precio;
+    var porcentajeDescuento;
+    var descuento;
+    var precioFinal; 
 
+    precio = prompt("Ingresar el precio del producto");
+    precio = parseInt(precio);
+    porcentajeDescuento = prompt("Ingresar porcentaje de descuento");
+
+    descuento = precio * porcentajeDescuento / 100;
+    precioFinal = precio - descuento;
+
+    document.getElementById("elPrecioFinal").value=precioFinal;
 }
