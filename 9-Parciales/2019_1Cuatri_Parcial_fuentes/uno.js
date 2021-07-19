69feef63be2afefbef1f8545721b58a0ace346dd
Sharlenne <sharlenne8@gmail.com>
2021-07-18 21:59:23 -0300

    Cambio Nuevo

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+    var lado;
+    var perimetro;
+    var mensaje;
+
+    lado = prompt("Ingresar la medida de los lados");
+    lado = parseInt(lado);
+
+    perimetro = lado * 3;
+
+    mensaje = "El perímetro del triángulo es "+perimetro;
+
+    alert(mensaje);
 
 }
