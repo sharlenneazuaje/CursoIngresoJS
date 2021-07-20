a531eea17a8cf8a43cff280adbfa82da1351e028
Sharlenne <sharlenne8@gmail.com>
2021-07-20 14:09:05 -0300

    Cambio Nuevo

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,82 @@
 function mostrar()
 {
+    var continente;
+    var cantidadDías;
+    var metodoPago;
+    var precioBaseDia;
+    var porcentaje;
+    var mensaje;
+    var precioInicial;
+    var precioFinal;
+
+    continente = document.getElementById("Marca").value;
+
+    cantidadDías = prompt("Ingresar la cantidad de días de viaje");
+    cantidadDías = parseInt(cantidadDías);
+    metodoPago = prompt("Ingresar el método de pago","débito , mercadoPago, efectivo, etc.");
+
+    precioBaseDia = 100;
+
+    switch(continente)
+    {
+        case "América":
+            switch(metodoPago)
+            {
+                case "débito":
+                    porcentaje = -60;
+                    break;
+                default:
+                    porcentaje = -50;
+                    break;
+            }
+            break;
+        
+        case "África":
+            switch(metodoPago)
+            {
+                case "mercadoPago":
+                case "efectivo":
+                    porcentaje = -75;
+                    break;
+                default:
+                    porcentaje = -60;
+                    break;
+            }
+            break;
+        
+        case "Europa":
+            switch(metodoPago)
+            {
+                case "débito":
+                    porcentaje = -35;
+                    break;
+                case "mercadoPago":
+                    porcentaje = -30;
+                    break;
+                case "efectivo": //el "otro" medio de pago
+                    porcentaje = -25;
+                    break;
+                default: // Pago con crédito, por ejemplo
+                    porcentaje = -20;
+                    break;
+            }
+            break;
+
+        default:
+            switch(metodoPago)
+            {
+                default:
+                    porcentaje = +20;
+                    break;
+            }
+            break;
+    }
+
+    precioInicial= precioBaseDia * cantidadDías;
+    precioFinal = precioInicial + precioInicial * porcentaje / 100;
+
+    mensaje = "El precio total a pagar por el viaje es "+precioFinal;
+
+    alert(mensaje);
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var suma;
+    var resta;
+    var mensaje;
 
+    numeroUno = prompt("Ingresar un número");
+    numeroUno = parseInt(numeroUno);
+    numeroDos = prompt("Igresar un número");
+    numeroDos = parseInt(numeroDos);
+
+    if(numeroUno == numeroDos)
+    {
+        mensaje = "Los números concatenados son "+numeroUno+""+ numeroDos;
+    }
+    else
+    {
+        if(numeroUno > numeroDos)
+        {
+            resta = numeroUno - numeroDos;
+            mensaje = "La resta de ambos números es "+resta;
+
+            if(resta > 10)
+            {
+                mensaje = "La resta es "+resta+" y superó el 10.";
+            }
+        }
+        else
+        {
+            suma = numeroDos + numeroUno;
+            mensaje = "La suma de ambos números es "+suma;
+        }
+    }
+    alert(mensaje);
+    
 }
